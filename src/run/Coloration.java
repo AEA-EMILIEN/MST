dd590c7c4206073cabde2f4891d5beb856c117f2
Tyboon <emilie.tyboon@gmail.com>
2015-04-10 09:30:50 +0200

    coloration naive, methode getColor

--- src/run/Coloration.java
+++ src/run/Coloration.java
@@ -4,11 +4,57 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import exception.VertexNotFoundException;
 
 import graph.Graph;
 
 public class Coloration {
 	
+	private ArrayList<Integer> listColor = new ArrayList<Integer>();
+	private HashMap<Integer,Integer> colorVertex = new HashMap<Integer, Integer>();
+	
+	public int getColor(int v, Set<Integer> listN) {
+		int[] colors = new int[this.listColor.size()];
+		int cpt = 0;
+		// initialise à -1 le tableau de couleurs
+		for (int i= 0; i < colors.length; i++)
+			colors[i] = -1;
+		// met à 1 les couleurs utilisées par les sommets
+		for (int n : listN) 
+			colors[colorVertex.get(n)] = 1;
+		// cherche la première couleur non utilisée
+		while ( (colors[cpt] == -1) && (cpt < colors.length) )
+			cpt++;
+		// si elle existe la retourne
+		if (cpt < colors.length)
+			return cpt;
+		// sinon en ajoute une nouvelle
+		else {
+			listColor.add(cpt);
+			return cpt;
+		}
+	}
+	
+	/**
+	 * Méthode naïve de coloration de des graphes
+	 * @param g le graphe à colorer, pré-requis les poids sont à -1
+	 * @return g le graphe coloré
+	 * @throws VertexNotFoundException 
+	 */
+	public HashMap<Integer, Integer> naif(Graph g) throws VertexNotFoundException {
+		this.listColor = new ArrayList<Integer>();
+		this.colorVertex = new HashMap<Integer, Integer>();
+		
+		for (int v = 0; v < g.vertex.size(); v++) {
+			Set<Integer> listN = g.getVertex(v).keySet();
+			int color = getColor(v, listN);
+			this.colorVertex.put(v, color);
+		}
+		
+		return this.colorVertex;
+	}
 	
 	public Graph runWelshPowell(Graph g)
 	{
