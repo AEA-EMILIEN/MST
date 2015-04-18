1e1e7ee56d8d4ebd2089cffdd81a74b40e8e2f22
JulienRouse <julien.rouse@gmail.com>
2015-04-18 19:35:17 +0200

    graphiques

--- plot.py
+++ plot.py
@@ -1,10 +1,20 @@
 import matplotlib.pyplot as plt
 
-def plotFunction(x,data):
+def plot2Function(x,dataPrim, dataKruskal):
     
-    plt.plot([1,2,3,4],[1,4,9,16])
-    plt.plot([1,2,3,4],[2,4,6,8])
-    plt.ylabel('some numbers')
+    y = []
+
+    for i in range(1,len(dataPrim)):
+        y = dataPrim[i].split()
+       
+        plt.plot(x,y,"--",label="prim, p="+str((i-1)*2*0.1+0.1))
+        y = dataKruskal[i].split()
+       
+        plt.plot(x,y,"-.",label="kruskal, p="+str((i-1)*2*0.1+0.1))
+        
+    plt.legend(bbox_to_anchor=(0., 1.), loc=2, borderaxespad=0.)
+    plt.xlabel('nombre de sommets dans le graphe')
+    plt.ylabel('temps en milliseconde d"execution de l"algorithme')
     plt.show()
 
 
@@ -15,14 +25,14 @@ def load_data(filename):
     
     x = content[0].split()
     x = range(int(x[0]),int(x[1])+int(x[2]),int(x[2]))
-    toto=[]
-
-    for l in content:
-       toto+= l.split()
     
-    return x,toto
+
+    
+    return x,content
 
 if __name__ == '__main__':
     x,data = load_data("data/Prim")
+    x2,data2 = load_data("data/KruskalTime")
+
+    plot2Function(x,data,data2)
     
-    print data
